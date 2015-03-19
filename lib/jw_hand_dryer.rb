25dc459b621d8b8969b743265d7c047458fb10ad
Jay Wengrow <jaywngrw@gmail.com>
2015-03-19 18:41:38 -0500

    initial commit

---
+++ lib/jw_hand_dryer.rb
@@ -0,0 +1,9 @@
+require "jw_hand_dryer/version"
+
+module JwHandDryer
+  class Dryer
+    def dry
+      `say ooooooooooooooooooooooooooo ooooooooooooooooooooo`
+    end
+  end
+end
---
+++ lib/jw_hand_dryer/version.rb
@@ -0,0 +1,3 @@
+module JwHandDryer
+  VERSION = "0.0.1"
+end
